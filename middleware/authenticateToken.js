96c4183ea12440329e0fd330811c82464cc6a9b6
V1etDuy <tdv0905179758@gmail.com>
2024-11-18 22:47:50 +0700

    fix verify cart

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -4,14 +4,20 @@ const SECRET_KEY = process.env.SECRET_KEY;
 const authenticateToken = (req, res, next) => {
   const token =
     req.headers["authorization"] && req.headers["authorization"].split(" ")[1];
-  if (!token) return res.status(401).json({ message: "No token provided" }); // Không có token
+  if (!token) {
+    console.log("No token provided");
+    return res.status(401).json({ message: "No token provided" }); // Không có token
+  
+  }
 
   jwt.verify(token, SECRET_KEY, (err, user) => {
     if (err) {
       // Kiểm tra xem lỗi có phải là do token hết hạn hay không
       if (err.name === "TokenExpiredError") {
+        console.log("Token has expired");
         return res.status(401).json({ message: "Token has expired" }); // Token hết hạn
       }
+      console.log("Invalid token");
       return res.status(401).json({ message: "Invalid token" }); // Token không hợp lệ
     }
     req.user = user; // Lưu thông tin người dùng vào request
