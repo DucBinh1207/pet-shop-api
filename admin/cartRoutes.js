2cb031462ade7c43eddab4f0e4d0be3af8cc134b
V1etDuy <tdv0905179758@gmail.com>
2024-11-23 11:38:01 +0700

    add mange cart and order admin

---
+++ admin/cartRoutes.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+// Middleware để parse JSON body
+router.use(express.json());
+const { authenticateToken } = require("../middleware/authenticateToken");
+const { client } = require("../db");
+
+router.get("/admin/cartItems", authenticateToken, async (req, res) => {
+    const id_role = req.user.id_role;
+
+    // Kiểm tra quyền truy cập
+    if (id_role !== 2) { // Chỉ admin được phép truy cập
+        return res.status(403).json({ message: "Bạn không có quyền truy cập" });
+    }
+
+    const id_user = req.query.userId || ""; // Lấy id_user từ query (nếu có)
+    const limit = parseInt(req.query.limit, 10) || 10; // Mặc định lấy 10 cart items mỗi trang
+    const page = parseInt(req.query.page, 10) || 1; // Mặc định là trang 1
+
+    console.log({ id_user });
+    console.log({ limit });
+    console.log({ page });
+
+    try {
+        await client.connect();
+        const db = client.db("PBL6"); // Kết nối tới database "PBL6"
+        const cartItemsCollection = db.collection("cart_items"); // Truy cập vào collection 'cart_items'
+
+        // Tạo filter nếu có id_user
+        let filter = {};
+        if (id_user) {
+            filter.id_user = id_user;
+        }
+
+        // Tính toán skip và limit
+        const skip = (page - 1) * limit;
+
+        // Lấy cart items theo filter và phân trang
+        const cartItems = await cartItemsCollection
+            .find(filter)
+            .skip(skip)
+            .limit(limit)
+            .toArray();
+
+        // Đếm tổng số cart items (không bị ảnh hưởng bởi skip/limit)
+        const totalCartItems = await cartItemsCollection.countDocuments(filter);
+        const totalPages = Math.ceil(totalCartItems / limit);
+
+        res.status(200).json({
+            cartItems,
+            //totalCartItems,
+            totalPages,
+            currentPage: page,
+        }); // Trả về danh sách cart items kèm thông tin phân trang
+    } catch (error) {
+        console.error("Error loading cart items:", error); // In ra lỗi nếu có
+        res.status(500).json({ message: "Lỗi máy chủ", error });
+    } finally {
+        await client.close(); // Đảm bảo đóng kết nối sau khi xử lý xong
+    }
+});
+
+
+
+module.exports = router;
